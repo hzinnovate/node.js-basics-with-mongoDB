3261fec814eaae64350f1bf9db34e178f10e83ae
Syed Hasnain Zaidi <39019111+syedzaidi95@users.noreply.github.com>
2020-01-11 03:15:20 +0500

    Update db.js
    add password against H--95 in Config

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongiURI = "mongodb+srv://hasnain:Haider95@cluster0-opxki.mongodb.net/test?retryWrites=true&w=majority"
+const mongiURI = "mongodb+srv://hasnain:H--95@cluster0-opxki.mongodb.net/test?retryWrites=true&w=majority"
 
 mongoose.connect(mongiURI);
 
